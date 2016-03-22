6f7245f2b73c661e99798122600d57219048cb41
Charlie <sgvvannabe@gmail.com>
2016-03-22 06:27:17 +0000

    Allowing Course Creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +61,4 @@ Flixter::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
