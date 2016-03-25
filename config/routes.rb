dbb60c241a27c3eb1c6a3f9fe0c91b56517a4e40
Charlie <sgvvannabe@gmail.com>
2016-03-25 07:10:44 +0000

    Setting up the Course Detail & Video Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
@@ -12,7 +13,6 @@ Flixter::Application.routes.draw do
     end
   end
 
-end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
