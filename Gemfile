915e120c4b13b81da22c38df6f4021bb11be000e
Charlie <sgvvannabe@gmail.com>
2016-05-04 07:39:37 +0000

    Allowing users to re-order lessons

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'carrierwave-aws'
 gem 'figaro'
 gem 'stripe'
 gem 'ranked-model'
+gem 'jquery-ui-rails'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Flixter::Application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :lessons, only: [:update]
     resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+    resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
